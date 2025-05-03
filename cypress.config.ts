bfbe5268a73fea1d738bbb48a536283c35fc4aae
dawidpodsiadly <dawid.podsiadly@interia.pl>
2025-05-03 22:13:59 +0200

    Run prettier

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,13 @@
-const { defineConfig } = require("cypress");
+const {defineConfig} = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    "baseUrl": "http://the-internet.herokuapp.com/",
-    "viewportWidth": 1920,
-    "viewportHeight": 1080,
-    "chromeWebSecurity": false,
-    "downloadsFolder": "cypress/downloads",
-    "trashAssetsBeforeRuns": true,
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://the-internet.herokuapp.com/',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    chromeWebSecurity: false,
+    downloadsFolder: 'cypress/downloads',
+    trashAssetsBeforeRuns: true,
   },
 });

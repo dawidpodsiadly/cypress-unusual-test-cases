dc1ce52589007d3dbf8a8df5e9a6fade4ff13c47
dawidpodsiadly <dawid.podsiadly@interia.pl>
2025-05-03 22:55:48 +0200

    Added File Uploader Case

---
+++ cypress/e2e/file-uploader.cy.ts
@@ -0,0 +1,17 @@
+describe('File Uploader', () => {
+    beforeEach(() => {
+      cy.visit('/upload');
+    });
+  
+    it('Upload File using Input', () => {    
+      cy.get('#file-upload').selectFile('cypress/downloads/upload-test-file.jpg', {action: 'select'});
+      cy.get('#file-submit').click();
+      cy.get('#uploaded-files').should('contain.text', 'upload-test-file.jpg');
+    });
+
+    it('Upload File using Drag and Drop', () => {    
+        cy.get('#drag-drop-upload').selectFile('cypress/downloads/upload-test-file.jpg', {action: 'drag-drop'});
+        cy.get('#drag-drop-upload').should('contain.text', 'upload-test-file.jpg');
+      });
+  });
+  

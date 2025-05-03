7f1a30912e2423f42392e725a8f5f9d36a1c31b7
dawidpodsiadly <dawid.podsiadly@interia.pl>
2025-05-03 18:32:27 +0200

    Update A/B Testing case

--- cypress/e2e/ab-testing.cy.ts
+++ cypress/e2e/ab-testing.cy.ts
@@ -1,18 +1,20 @@
-// Also known as split testing. This is a way in which businesses are able to simultaneously test and learn 
-// different versions of a page to see which text and/or functionality works best towards a desired outcome 
-// (e.g. a user action such as a click-through).
-
-
-it('A/B Test Control - variation A', () => {
+describe('A/B Testing', () => {
+  beforeEach(() => {
     cy.visit('/abtest');
+  });
+
+  it('A/B Test - variation A', () => {
     cy.setCookie('optimizelyBuckets', '%7B%22298349752%22%3A%22298291000%22%7D');
     cy.setCookie('optimizelyEndUserId', 'oeu1665296421799r0.1092416905447644');
+    cy.reload();
     cy.contains('h3', 'A/B Test Control').should('be.visible');
-  })
+  });
 
-it('A/B Test Control - variation B', () => {
-    cy.visit('/abtest');
+  it('A/B Test - variation B', () => {
     cy.setCookie('optimizelyBuckets', '%7B%22298349752%22%3A%22298343790%22%7D');
     cy.setCookie('optimizelyEndUserId', 'oeu1665349048094r0.1503419388184688');
+    cy.reload();
     cy.contains('h3', 'A/B Test Variation 1').should('be.visible');
-  })
+  });
+});
+

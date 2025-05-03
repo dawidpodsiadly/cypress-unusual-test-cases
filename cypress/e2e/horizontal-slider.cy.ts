42b16f1894e72665972615d218067e28bb9014dd
dawidpodsiadly <dawid.podsiadly@interia.pl>
2025-05-04 00:17:29 +0200

    Added Horizontal Slider Case

---
+++ cypress/e2e/horizontal-slider.cy.ts
@@ -0,0 +1,14 @@
+describe('Horizontal Slider', () => {
+    beforeEach(() => {
+      cy.visit('/horizontal_slider');
+    });
+  
+    it('Slider Should Change Value when Moving', () => {
+        cy.get('#range').should('have.text', '0');
+        cy.get('div.sliderContainer input').invoke('val', 3.5).trigger('change');
+        cy.get('#range').should('have.text', '3.5');
+        cy.get('div.sliderContainer input').invoke('val', 1).trigger('change');
+        cy.get('#range').should('have.text', '1');
+    });    
+  });
+  

55ea8f8930efc3cf116d76d393b813eb12ade4c7
dawidpodsiadly <dawid.podsiadly@interia.pl>
2025-05-04 22:01:29 +0200

    Added Infinite Scroll Case

--- cypress/e2e/infinite-scroll.cy.ts
+++ cypress/e2e/infinite-scroll.cy.ts
@@ -3,7 +3,7 @@ describe('Infinite Scroll', () => {
     cy.visit('/infinite_scroll');
   });
 
-  it('When Scrolling Down the Page a new Section Should Appear.', () => {
+  it('When Scrolling Down the Page a new Section Should Appear', () => {
     cy.get('div.jscroll-added').should('have.length', 2);
     cy.scrollTo('bottom');
     cy.get('div.jscroll-added').should('have.length', 3);

7fb887a00bad888e86075e14c2b84bd50791d0fc
dawidpodsiadly <dawid.podsiadly@interia.pl>
2025-05-04 23:19:15 +0200

    Added Notification Message Case

---
+++ cypress/e2e/notification-messages.cy.ts
@@ -0,0 +1,10 @@
+describe('Notification Message', () => {
+  beforeEach(() => {
+    cy.visit('/notification_message_rendered');
+  });
+
+  it('Notification Should be Successful', () => {
+    cy.get('div.example a').click();
+    cy.get('#flash').should('have.text', 'Action successful');
+  });
+});

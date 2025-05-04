7c14d39311dbe365d0a392af9e580290ec94e776
dawidpodsiadly <dawid.podsiadly@interia.pl>
2025-05-04 23:27:40 +0200

    Added Redirect Link Case

---
+++ cypress/e2e/redirect-link.cy.ts
@@ -0,0 +1,29 @@
+describe('Redirect Link', () => {
+  beforeEach(() => {
+    cy.visit('/redirector');
+  });
+
+  it('Redirection - 200 Status Code', () => {
+    cy.get('#redirect').click();
+    cy.get('a').contains('200').click();
+    cy.get('#content').should('contain.text', 'This page returned a 200 status code.');
+  });
+
+  it('Redirection - 301 Status Code', () => {
+    cy.get('#redirect').click();
+    cy.get('a').contains('301').click();
+    cy.get('#content').should('contain.text', 'This page returned a 301 status code.');
+  });
+
+  it('Redirection - 404 Status Code', () => {
+    cy.get('#redirect').click();
+    cy.get('a').contains('404').click();
+    cy.get('#content').should('contain.text', 'This page returned a 404 status code.');
+  });
+
+  it('Redirection - 500 Status Code', () => {
+    cy.get('#redirect').click();
+    cy.get('a').contains('500').click();
+    cy.get('#content').should('contain.text', 'This page returned a 500 status code.');
+  });
+});
